41b64251e3344fc4bf08d739a86f528b571250d3
AzzieRobel <wibipusahaf862@gmail.com>
2026-01-12 00:54:18 +0900

    Create Reverseadoublylinkedlist.py

---
+++ Data Structures/Reverseadoublylinkedlist.py
@@ -0,0 +1,105 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def reverse(llist):
+    """
+    Reverse a doubly linked list in place and return the new head.
+    
+    Args:
+        llist: head node of the doubly linked list
+        
+    Returns:
+        DoublyLinkedListNode: new head of the reversed list
+    """
+    # Handle empty list
+    if not llist:
+        return None
+    
+    current = llist
+    new_head = None
+    
+    # Traverse the list and swap next/prev pointers for each node
+    while current:
+        # Swap next and prev pointers
+        current.next, current.prev = current.prev, current.next
+        
+        # Move to the next node (which is now prev due to swap)
+        new_head = current  # Track the last processed node as new head
+        current = current.prev  # Move to what was originally the next node
+    
+    return new_head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
