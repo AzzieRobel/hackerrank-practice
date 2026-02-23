934962fe5ed2aed58c821ec824fe084f039e0917
AzzieRobel <wibipusahaf862@gmail.com>
2026-02-24 02:57:37 +0900

    Create SalesbyMatch.py

---
+++ algorithms/SalesbyMatch.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    count = {}
+    pairs = 0
+    
+    for sock in ar:
+        if sock in count:
+            count[sock] += 1
+        else:
+            count[sock] = 1
+    
+    for value in count.values():
+        pairs += value // 2
+    
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
