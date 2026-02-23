54ed0f72dcae076603cb0cb2986f8196142840eb
AzzieRobel <wibipusahaf862@gmail.com>
2026-02-24 06:36:10 +0900

    Create ForLoopinC.c

---
+++ C/ForLoopinC.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Loop through the range from a to b
+    for (int i = a; i <= b; i++) {
+        // Handle numbers 1 through 9 with their English representation
+        if (i >= 1 && i <= 9) {
+            switch(i) {
+                case 1: printf("one\n"); break;
+                case 2: printf("two\n"); break;
+                case 3: printf("three\n"); break;
+                case 4: printf("four\n"); break;
+                case 5: printf("five\n"); break;
+                case 6: printf("six\n"); break;
+                case 7: printf("seven\n"); break;
+                case 8: printf("eight\n"); break;
+                case 9: printf("nine\n"); break;
+            }
+        } else {
+            // Handle numbers greater than 9
+            if (i % 2 == 0) {
+                printf("even\n");
+            } else {
+                printf("odd\n");
+            }
+        }
+    }
+
+    return 0;
+}
